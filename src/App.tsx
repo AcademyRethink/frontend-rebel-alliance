99dc93b1085be90e95dcb886c41cfebe345e4ee9
thiagoFirsen <thiago.firsen@rethink.dev>
2023-07-03 14:59:13 -0300

    feate: login layout creation  with Skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Login from "./screens/ExampleScreen/Login";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Login />
+    </>
+  );
 }
 
 export default App;
