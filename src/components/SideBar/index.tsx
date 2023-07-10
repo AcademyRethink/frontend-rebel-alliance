8628ae384906053acda003f5bec7838b7438306a
thiagoFirsen <thiago.firsen@rethink.dev>
2023-07-10 17:20:18 -0300

    feat: sidebar component with initial routes

---
+++ src/components/SideBar/index.tsx
@@ -0,0 +1,70 @@
+import "./styles.scss";
+import monitoreLogo from "./../../assets/sideBar/monitoreLogo.svg";
+import iconFarm from "./../../assets/sideBar/iconFarm.svg";
+import iconClima from "./../../assets/sideBar/iconClima.svg";
+import iconPerfil from "./../../assets/sideBar/iconPerfil.svg";
+import iconExit from "./../../assets/sideBar/iconExit.svg";
+import Button from "../Button";
+import { NavLink } from "react-router-dom";
+
+const SideBar = () => {
+  return (
+    <div className="sideBar">
+      <div className="containerLogo">
+        <img src={monitoreLogo} alt="Logo monitore" />
+      </div>
+      <nav className="containerButtons">
+        <ul className="containerLinks">
+          <NavLink
+            to="/home"
+            className={({ isActive }) =>
+              isActive ? "linkActive" : "linkNoActive"
+            }
+          >
+            <Button
+              className="sideBarButton"
+              text="Fazenda"
+              iconLeft={iconFarm}
+            />
+          </NavLink>
+
+          <NavLink
+            to="/clima"
+            className={({ isActive }) =>
+              isActive ? "linkActive" : "linkNoActive"
+            }
+          >
+            <Button
+              className="sideBarButton"
+              text="Clima"
+              iconLeft={iconClima}
+            />
+          </NavLink>
+
+          <NavLink
+            to="/perfil"
+            className={({ isActive }) =>
+              isActive ? "linkActive" : "linkNoActive"
+            }
+          >
+            <Button
+              className="sideBarButton"
+              text="Perfil"
+              iconLeft={iconPerfil}
+            />
+          </NavLink>
+        </ul>
+        <div className="containerExit">
+          <Button
+            className="sideBarButton"
+            text="Sair"
+            iconLeft={iconExit}
+            onClick={() => console.log("Sair")}
+          />
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default SideBar;
