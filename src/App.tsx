48de385ebd4812fd224b527eb0872dbb55925132
DuarteJoseGeraldo <124914532+DuarteJoseGeraldo@users.noreply.github.com>
2023-07-11 15:00:46 -0300

    fix/ limpando main para PR

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
 import "./App.scss";
-import TextInput from "./components/Input";
 
 function App() {
-  return (
-    <>
-      <TextInput
-        className="Default"
-        label="Label"
-        placeHolder="Placeholder"
-        errorMessage="Mensagem de erro"
-      />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
