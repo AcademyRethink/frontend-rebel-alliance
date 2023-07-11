b0ccb7efc29d74535c16b6d1f22027d638ed2f88
thiagoFirsen <thiago.firsen@rethink.dev>
2023-07-11 16:49:37 -0300

    feat: title component complete

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,6 +1,8 @@
 import { TitleProps } from "../../types/titleTypes";
+import { NavLink } from "react-router-dom";
 import "./styles.scss";
 import iconArrow from "./../../assets/Title/icon_arrow.svg";
+
 const Title = ({
   fontSize,
   fontWeight,
@@ -11,9 +13,9 @@ const Title = ({
   return (
     <div className="containerTitle">
       {hasIcon && (
-        <button onClick={() => history.back()}>
+        <NavLink to="/home">
           <img src={iconArrow} alt="icone para voltar" />
-        </button>
+        </NavLink>
       )}
       <h1 style={{ fontSize: fontSize, fontWeight: fontWeight }}>{text}</h1>
       {hasLine && <div className="lineTitle"></div>}
