2696abc283d7d81aef7bbf78f5f32a952a124515
thiagoFirsen <thiago.firsen@rethink.dev>
2023-07-12 18:18:14 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,7 @@
 import "./App.scss";
-import Climate from "./screens/ExampleScreen/Climate";
-import Home from "./screens/ExampleScreen/Home";
-import Login from "./screens/ExampleScreen/Login";
-import Plot from "./screens/ExampleScreen/Plot";
-import Profile from "./screens/ExampleScreen/Profile";
 
 function App() {
-  return (
-    <>
-      <Login />
-      <Home />
-      <Profile />
-      <Plot />
-      <Climate />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
