93c0646d80b8eb69cf92a75eb6c88ff89c1ea482
thiagoFirsen <thiago.firsen@rethink.dev>
2023-07-12 18:09:55 -0300

    feat: component login page plot

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.scss";
 import Home from "./screens/ExampleScreen/Home";
 import Login from "./screens/ExampleScreen/Login";
+import Plot from "./screens/ExampleScreen/Plot";
 import Profile from "./screens/ExampleScreen/Profile";
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
     <>
       {/* <Login />
       <Home /> */}
-      <Profile />
+      {/* <Profile /> */}
+      <Plot />
     </>
   );
 }
