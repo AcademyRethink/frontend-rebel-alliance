2df139a25973ec05cef7df439e9acdf9fe6379eb
ribYuriRethink <yuri.maycon@rethink.dev>
2023-07-17 12:26:12 -0300

    refactor: app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,13 @@ import "./App.scss";
 import Home from "./pages/Home";
 import Climate from "./pages/Climate";
 import Profile from "./pages/Profile";
-import WeatherWeek from "./components/WeatherWeek";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<WeatherWeek city="Belo Horizonte" />} />
+          <Route path="/" />
           <Route path="/home" element={<Home />} />
           <Route path="/plantio/:id" />
           <Route path="/clima" element={<Climate />} />
