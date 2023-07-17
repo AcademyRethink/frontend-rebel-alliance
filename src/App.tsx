21f0a686a0263272ec48d7dbac8106d36b5b9ff1
ribYuriRethink <yuri.maycon@rethink.dev>
2023-07-17 12:22:41 -0300

    refactor: remove component of app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,13 @@ import "./App.scss";
 import Home from "./pages/Home";
 import Climate from "./pages/Climate";
 import Profile from "./pages/Profile";
-import SummaryTime from "./components/SummaryTime";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<SummaryTime resume={false} />} />
+          <Route path="/" />
           <Route path="/home" element={<Home />} />
           <Route path="/plantio/:id" />
           <Route path="/clima" element={<Climate />} />
