5fec517004bbdd73cbcde935361c7dbc5e001e95
ribYuriRethink <yuri.maycon@rethink.dev>
2023-07-21 13:07:29 -0300

    feat: auth on profile page

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,18 +1,25 @@
+import "./styles.scss";
 import SideBar from "../../components/SideBar";
 import Title from "../../components/Title";
 import UserDatas from "../../components/UserDatas";
-import "./styles.scss";
+import { useContext } from "react";
+import { AuthContext } from "../../controllers/contextController";
 
 const Profile = () => {
-  return (
-    <div className="profileContainer">
-      <SideBar />
-      <div className="dataProfileContainer">
-        <Title fontSize="" fontWeight="" text="Perfil" hasIcon hasLine />
-        <UserDatas userID={1} />
+  const { userData } = useContext(AuthContext);
+  if (userData?.token) {
+    return (
+      <div className="profileContainer">
+        <SideBar />
+        <div className="dataProfileContainer">
+          <Title fontSize="" fontWeight="" text="Perfil" hasIcon hasLine />
+          <UserDatas userID={userData.info.id} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    window.location.href = "/";
+  }
 };
 
 export default Profile;
