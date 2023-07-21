77956b28ae0ada3604ce1d4c7593e40ad574b495
Leticia-Guimaraes <124922074+Leticia-Guimaraes@users.noreply.github.com>
2023-07-21 16:13:01 -0300

    Chore/*: changes

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,22 +1,9 @@
 import SideBar from "../../components/SideBar";
-import PlantingData from "../../components/PlantingData";
 
 const Home = () => {
-  const mock = {
-    farm_id: 23,
-    plot_id: 103,
-    plot_name: "Updated Plot 83",
-    planting_id: 183,
-    planting_date: "2023-06-26T00:00:00.000Z",
-    saplings: 603,
-    stage: "Florada",
-    stage_order: 4,
-    harvests: "33",
-  };
-
   return (
     <div>
-      <PlantingData plotData={mock} cultureID={1} />
+      <SideBar />
     </div>
   );
 };
