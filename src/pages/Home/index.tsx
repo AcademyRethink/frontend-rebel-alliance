680318a7f5a9e20171133aa3ef100de04811eb24
thiagoFirsen <thiago.firsen@rethink.dev>
2023-07-21 17:20:11 -0300

    update

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,32 @@
+import { useState, useEffect, useContext } from "react";
+import { getPlotgByFarmID } from "../../services/plot";
+import PlantingData from "../../components/PlantingData";
+import { AuthContext } from "../../controllers/contextController";
 import SideBar from "../../components/SideBar";
-import EditCard from "../../components/EditCard";
+import { PlotWithFarm } from "../../types/plotTypes";
 import "./styles.scss";
 
 const Home = () => {
+  const { userData } = useContext(AuthContext);
+
+  const [dataPlot, setDataPlot] = useState<Array<PlotWithFarm>>();
+
+  useEffect(() => {
+    getPlotgByFarmID(userData.info.farm_id)
+      .then((response) => {
+        setDataPlot(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
   return (
     <div className="pageClimateContainer">
       <SideBar />
       <div className="homeDataContainer">
-        <EditCard />
+        {dataPlot?.map((plot) => (
+          <PlantingData plotData={plot} cultureID={1} />
+        ))}
       </div>
     </div>
   );
