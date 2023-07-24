de40132cd0c37051afbb2925be7095e8adf99072
ribYuriRethink <yuri.maycon@rethink.dev>
2023-07-24 15:20:01 -0300

    fix: validate token loop

--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -1,12 +1,16 @@
 /* eslint-disable no-empty */
 import Login from "../../screens/Login";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../controllers/contextController";
 import Home from "../Home";
 const AuthPage = () => {
   const { userData, validateToken } = useContext(AuthContext);
-  if (localStorage.getItem("token"))
+  const [validate, setValidate] = useState<number>(1);
+
+  if (validate && localStorage.getItem("token")) {
     validateToken(localStorage.getItem("token"));
+    setValidate(0);
+  }
 
   if (userData?.token) return <Home />;
   else return <Login />;
