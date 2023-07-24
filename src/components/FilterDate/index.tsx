4f30aee248863a974caabc2ee569bdf1df9c7958
Leticia-Guimaraes <124922074+Leticia-Guimaraes@users.noreply.github.com>
2023-07-24 12:30:26 -0300

    Feat/*: New features

---
+++ src/components/FilterDate/index.tsx
@@ -0,0 +1,12 @@
+import "./styles.scss";
+import Button from "../Button";
+
+const FilterDate = () => {
+  return (
+    <div>
+      <Button className="" text="" iconLeft="" iconRight="" />
+    </div>
+  );
+};
+
+export default FilterDate;
